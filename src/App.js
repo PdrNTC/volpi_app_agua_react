a0a2db8ec099e686124c0a1183bb8c52d88cd923
Pedro Henrique Ernandes da Silva <thepedrinhoo@gmail.com>
2024-08-29 09:58:14 -0300

    Arquivos Iniciais do projeto APP Agua

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>VOLPI - APP AGUA REACT.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
